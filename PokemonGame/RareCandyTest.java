7200a67200c3959980fafd2ba233bd9f0c07c194
serialUmo <nasif.i.ahmed@gmail.com>
2024-10-28 15:26:24 -0400

    Tweak output

--- PokemonGame/RareCandyTest.java
+++ PokemonGame/RareCandyTest.java
@@ -9,8 +9,9 @@ public class RareCandyTest {
     public static void main(String[] args){
         MonteCarloSims sim = new MonteCarloSims();
         
-        for(int i = 1; i < 60; i++){
-            System.out.println(sim.rareCandySim(10000, i)/100);
+        for(int i = 1; i < 8; i++){
+            System.out.println("The chance of bricking with " + i + " candies is "
+                                 + sim.rareCandySim(1000000, i)/100);
         }
     }
 }
