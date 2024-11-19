636452db941d4ef303a034531e3bb44f20dff790
serialUmo <nasif.i.ahmed@gmail.com>
2024-11-19 12:21:30 -0500

    Start Project2

---
+++ Project2/Plotter.java
@@ -0,0 +1,28 @@
+package Project2;
+
+import java.util.ArrayList;
+
+public class Plotter
+{
+    public Plotter(){
+
+    }
+
+    /**
+     * Returns the value of x^3.
+     * @param x
+     */
+    public double formula(double x){
+        return Math.pow(x, 3);
+    }
+
+    /**
+     * Returns an array list of (x, y) values according to a formula.
+     * @param a The lower bound.
+     * @param b The upper bound.
+     * @return
+     */
+    public ArrayList<Double> plot(double a, double b){
+        
+    }
+}
